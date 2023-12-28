b48c2a3df9760c7c5f8484f3c53eed512d8d39db
Rogério Munhoz <rogerio.munhoz@pipefy.com>
2023-12-28 11:23:37 -0300

    fix: remove dependency on @grammyjs/types

--- src/deps.node.ts
+++ src/deps.node.ts
@@ -1,2 +1,2 @@
-export type { Chat, ChatMember, ChatMemberUpdated, User } from "@grammyjs/types";
 export { Composer, Context, type StorageAdapter } from "grammy";
+export type { Chat, ChatMember, ChatMemberUpdated, User } from "grammy/types";
