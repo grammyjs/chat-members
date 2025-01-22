e8bc14110081aa598e7b3882aaf7229760afa756
rayz1065 <rayz1065@gmail.com>
2025-01-22 17:37:05 +0100

    Moved existing code to separate file, added chat member filters

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,14 @@
-export { Composer, Context, type StorageAdapter } from "https://lib.deno.dev/x/grammy@v1/mod.ts";
-export type { Chat, ChatMember, ChatMemberUpdated, User } from "https://lib.deno.dev/x/grammy@v1/types.ts";
+export { Api, Composer, Context, type Filter, type StorageAdapter } from "https://lib.deno.dev/x/grammy@v1/mod.ts";
+export type {
+    Chat,
+    ChatMember,
+    ChatMemberAdministrator,
+    ChatMemberBanned,
+    ChatMemberLeft,
+    ChatMemberMember,
+    ChatMemberOwner,
+    ChatMemberRestricted,
+    ChatMemberUpdated,
+    User,
+    UserFromGetMe,
+} from "https://lib.deno.dev/x/grammy@v1/types.ts";
